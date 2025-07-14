b8ddd9933926cb35e9f2d4e021f1df1a65f27c6d
unknown <vishurudra@gmail.com>
2025-07-14 15:54:56 +0530

    done

--- eg1/src/App.js
+++ eg1/src/App.js
@@ -1,15 +1,31 @@
-import './App.css';
-import Nav from './components/Nav';
+import "./App.css";
+import Nav from "./components/Nav";
 
-import { useState } from 'react';
-import UserNameContext from './context/UserNameContext';
+import { useState } from "react";
+import UserNameContext from "./context/UserNameContext";
+import ThemeContext from "./context/ThemeContext";
 function App() {
-  const [authenticated,setAuthenticated]=useState(false)
-  const [username,setUserName] =useState("")
+  const [authenticated, setAuthenticated] = useState(false);
+  const [username, setUserName] = useState("");
+  const [backgroundColor, setBackgroundColor] = useState("");
+  const setBgColor=e=>setBackgroundColor(e.target.value)
   return (
     <div className="App">
-      <UserNameContext.Provider value={{username,setUserName}}>
-      <Nav authenticated={authenticated} setAuthenticated={setAuthenticated} />
+      <UserNameContext.Provider value={{ username, setUserName }}>
+       <ThemeContext.Provider value={{backgroundColor}}>
+        
+        <select onChange={setBgColor}>
+          <option>""</option>
+          <option value={"red"}>RED</option>
+          <option value={"green"}>GREEN</option>
+          <option value={"yellow"}>YELLOW</option>
+        </select>
+        <Nav
+          authenticated={authenticated}
+          setAuthenticated={setAuthenticated}
+        />
+        
+       </ThemeContext.Provider>
       </UserNameContext.Provider>
     </div>
   );
--- eg1/src/components/functional/student/StudentTable.js
+++ eg1/src/components/functional/student/StudentTable.js
@@ -1,14 +1,16 @@
 import { useContext } from "react";
 import UserNameContext from "../../../context/UserNameContext";
+import ThemeContext from "../../../context/ThemeContext";
 export default function StudentTable({ students, color }) {
   //instead of using props everywhere u can make use of {property]}
   const userNameContext = useContext(UserNameContext);
+  const themeContext=useContext(ThemeContext)
   if (students) {
     if (students.length > 0) {
       return (
         <div>
           <h1>Welcome {userNameContext.username}</h1>
-          <table style={{ backgroundColor: color }} border={1}>
+          <table style={{ backgroundColor: themeContext.backgroundColor }} border={1}>
             <thead>
               <tr>
                 <th>Roll No</th>
