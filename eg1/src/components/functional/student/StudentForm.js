1313f7ddc30a78e01d0f6ae42e4877d47f0d5239
unknown <vishurudra@gmail.com>
2025-07-14 13:21:04 +0530

    done

--- eg1/src/components/functional/student/StudentForm.js
+++ eg1/src/components/functional/student/StudentForm.js
@@ -4,15 +4,27 @@ import StudentTable from "./StudentTable";
 const StudentForm = () => {
   const [formData, setFormData] = useState({ rollNo: -1, name: "" });
   const [students, setStudents] = useState([]);
-    const addStudent =e =>{
-        e.preventDefault()//prevent the form from its core behaviour of going to url
-        setStudents(students.concat(formData))
+  const [enableButton, setEnableButton] = useState(false);
+  const addStudent = (e) => {
+    e.preventDefault(); //prevent the form from its core behaviour of going to url
+    setStudents(students.concat(formData));
+  };
+  const capture = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setFormData({ ...formData, [name]: value });
+    if (name === "rollNo") {
+      if (formData.name.length > 0) {
+        setEnableButton(true);
+      }
+    } else {
+      if (name === "name") {
+        if (formData.rollNo > -1) {
+          setEnableButton(true);
+        }
+      }
     }
-    const capture =e =>{
-        let name =e.target.name
-        let value =e.target.value
-        setFormData({...formData,[name]:value})
-    }
+  };
   return (
     <div>
       <form onSubmit={addStudent}>
@@ -21,12 +33,14 @@ const StudentForm = () => {
         </label>
         <br />
         <label>
-          Enter Name <input name="name" required onChange={capture}/>
+          Enter Name <input name="name" required onChange={capture} />
         </label>
-        <br/>
-        <button>Add Student</button>
+        <br />
+        <button type="submit" disabled={!enableButton}>
+          Add Student
+        </button>
       </form>
-      <StudentTable students={students} color="yellow"/>
+      <StudentTable students={students} color="yellow" />
     </div>
   );
 };
